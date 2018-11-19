c6db9f5dd70caa124fda934875a4ddf621d3aa53
Tsuribori <tsuribori@tutanota.com>
2018-11-19 19:17:13 +0200

    Fixed typos that prevented user settings coming to effect

--- rest_validator/conf/settings.py
+++ rest_validator/conf/settings.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-REST_VALIDATOR_CACHE_TIMEOUT = getattr(settings, 'REST_CAPTCHA_VALIDATOR_TIMEOUT', 300)
-REST_VALIDATOR_SINGLE_USE = getattr(settings, 'REST_VALIDATOR_SINGE_USE', True)
+REST_VALIDATOR_CACHE_TIMEOUT = getattr(settings, 'REST_VALIDATOR_CACHE_TIMEOUT', 300)
+REST_VALIDATOR_SINGLE_USE = getattr(settings, 'REST_VALIDATOR_SINGLE_USE', True)
 CAPTCHA_TIMEOUT = getattr(settings, 'CAPTCHA_TIMEOUT', 5) #5 is the default CAPTCHA_TIMEOUT of Django Simple Captcha
 
